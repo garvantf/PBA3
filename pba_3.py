44c2d543a15c0d0527808d0925ce5613a03c9fab
garvantf <125227541+garvantf@users.noreply.github.com>
2024-06-28 04:15:40 +0700

    Update pba_3.py

--- pba_3.py
+++ pba_3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import re
 from collections import Counter
-import time
 
 # Fungsi untuk memuat korpus
 def load_corpus(path):
@@ -32,34 +31,35 @@ def candidates(word):
 def known(words):
     return set(w for w in words if w in WORDS)
 
-# Fungsi untuk mendapatkan semua edit satu langkah dari kata
+# Fungsi untuk menghasilkan semua kata yang berjarak satu edit dari kata yang diberikan
 def edits1(word):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes = [L + R[1:] for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
-    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
-    inserts = [L + c + R for L, R in splits for c in letters]
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
-# Fungsi untuk mendapatkan semua edit dua langkah dari kata
+# Fungsi untuk menghasilkan semua kata yang berjarak dua edit dari kata yang diberikan
 def edits2(word):
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-# Streamlit UI
+# Judul aplikasi
 st.title("Indonesian Spell Checker using Norvig's Algorithm")
 
-input_words = st.text_input("Masukkan kata-kata yang dipisahkan dengan koma:", "")
+# Input teks dari pengguna
+input_text = st.text_area("Masukkan kata yang ingin diperiksa (pisahkan dengan koma):")
 
-if st.button("Periksa Ejaan"):
-    test_words = input_words.split(',')
-    
-    start = time.time()
-    corrected_words = {w: correction(w.strip()) for w in test_words}
-    end = time.time()
-    
-    st.write(f'Waktu yang diperlukan: {end - start:.4f} detik')
-    
-    st.write("Koreksi Kata:")
-    for word, correction in corrected_words.items():
-        st.write(f"'{word.strip()}' -> '{correction}'")
+if st.button("Periksa"):
+    if input_text:
+        test_words = input_text.split(',')
+        results = {}
+        for w in test_words:
+            results[w] = correction(w.strip())
+        # Tampilkan hasil
+        st.write("Hasil Koreksi:")
+        for w, corrected in results.items():
+            st.write(f"'{w.strip()}' -> '{corrected}'")
+    else:
+        st.write("Mohon masuk
