7329a462853a5c96a295297e85f74d3dd8739146
garvantf <125227541+garvantf@users.noreply.github.com>
2024-06-28 10:21:32 +0700

    Update pba_3.py

--- pba_3.py
+++ pba_3.py
@@ -37,8 +37,8 @@ def edits1(word):
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
     deletes    = [L + R[1:]               for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R for c in letters if R]
-    inserts    = [L + c + R               for L, R for c in letters]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
 # Fungsi untuk menghasilkan semua kata yang berjarak dua edit dari kata yang diberikan
